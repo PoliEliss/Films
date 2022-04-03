0c89be5c332ff69a9c00c7e1deabd58c2825ba6f
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-04-03 11:32:44 +0300

    get responce https://api.themoviedb.org/

---
+++ app/src/main/java/com/rorono/films/ItemViewModel.kt
@@ -0,0 +1,3 @@
+package com.rorono.films
+
+data class ItemViewModel(val image:Int,val text:String)
--- app/src/main/java/com/rorono/films/MoviesActivity.kt
+++ app/src/main/java/com/rorono/films/MoviesActivity.kt
@@ -1,13 +1,62 @@
 package com.rorono.films
 
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MoviesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_rmovies)
+
+        //getting the recyclerview by its id
+        val recyclerview = findViewById<RecyclerView>(R.id.recyclerviewMovies)
+
+        // this creates a vertical layout Manager (производится настройка)
+        recyclerview.layoutManager = GridLayoutManager(this,2)
+
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<ItemViewModel>()
+
+        // This loop will create 20 Views containing
+        // the image with the count of view
+        for (i in 1..20) {
+            data.add(ItemViewModel(R.drawable.common_full_open_on_phone, "Item " + i))
+        }
+
+
+
+
+        val apiInterface = ApiInterface.create().getMovies("12790cecfabd1bf1230c24684b802223")
+
+        //apiInterface.enqueue( Callback<List<Movie>>())
+        apiInterface.enqueue(object : Callback<Movies> {
+            override fun onResponse(call: Call<Movies>?, response: Response<Movies>?) {
+                Log.d("testLogs","OnREsponse Success ${response?.body()?.results}")
+
+                // This will pass the ArrayList to our Adapter
+                val adapter = CustomAdapter(response?.body()?.results)
+
+                // Setting the Adapter with the recyclerview
+                recyclerview.adapter = adapter
+
+             //   if (response?.body() != null)
+              //      recyclerAdapter.setMovieListItems(response.body()!!)
+            }
+
+            override fun onFailure(call: Call<Movies>?, t: Throwable?) {
+
+
+            }
+        })
+
+
     }
 
     override fun onBackPressed() {
